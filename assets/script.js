8748849bca3828ebd6fccd172a381c8a7f06d03e
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-02-10 10:09:07 -0500

    keys for api

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var key = "9f0ae0cdf7c18fc91720d7f062d45b91"
+var weatherApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + key;
+var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + key;
+
