a20c6e72bfeb10c4806de27df9faa91eaa06d054
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-02-10 11:02:35 -0500

    thing js is finished, need to style after i write the html

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,19 @@ var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{sta
 
 // looks to see if city exists in Local
 var storageCheck = function (searchItems, newCity) {
-    var notMatch = 0
+    var notAMatch = 0
     Object.values(searchItems).forEach(val => {
-      if (val === newCity) {
-        return;
-      } else {
-        return notMatch++;
-      }
+        if (val === newCity) {
+            return;
+        } else {
+            return notAMatch++;
+        }
     })
-    if (notMatch >= searchItems.length) {
-      searchItems.push(newCity);
-      localStorage.setItem("cities", JSON.stringify(searchItems));
+    if (notAMatch >= searchItems.length) {
+        searchItems.push(newCity);
+        localStorage.setItem("cities", JSON.stringify(searchItems));
     }
-  };
+};
 
 //   if not, this function adds city to local
 var storageAdd = function (lat, lon) {
@@ -24,15 +24,15 @@ var storageAdd = function (lat, lon) {
     var searchItems = JSON.parse(getSearches);
     var newCity = (lat + " " + lon);
     if (searchItems.length > 0) {
-      storageCheck(searchItems, newCity);
+        storageCheck(searchItems, newCity);
     } else {
-      searchItems.push(newCity);
-      localStorage.setItem("cities", JSON.stringify(searchItems));
+        searchItems.push(newCity);
+        localStorage.setItem("cities", JSON.stringify(searchItems));
     }
-  }
+}
 
 //   this is the forcast card
-  var displayWeather = function (data, location) {
+var displayWeather = function (data, location) {
     $('#weather-card').empty();
     var icon = (data.weather[0].icon);
     var city = (data.name + ', ' + location);
@@ -41,7 +41,6 @@ var storageAdd = function (lat, lon) {
     var feelsLike = (data.main.feels_like);
     var description = (data.weather[0].description);
     var wind = (data.wind.speed);
-  
     var weatherDiv = $('<div>').addClass('row');
     var weatherCard = $('<div>').addClass('card');
     var weatherCity = $('<h5>').addClass('card-title p-2');
@@ -51,142 +50,155 @@ var storageAdd = function (lat, lon) {
     var weatherWind = $('<li>').addClass('list-group-item');
     var weatherList = $('<ul>').addClass('list-group list-group-flush');
     var weatherHumidity = $('<li>').addClass('list-group-item');
-  
     weatherCity.text(city);
     weatherImage.attr('src', 'http://openweathermap.org/img/wn/' + icon + '@2x.png');
     weatherDescription.text('Currently: ' + temp + '\u00B0 with ' + description + '. (feels like ' + feelsLike + '\u00B0)');
     weatherHumidity.text('Humidity: ' + humidity + '%');
     weatherWind.text('Wind Speed: ' + wind + 'mph');
-  
+
     weatherList
-      .append(weatherHumidity)
-      .append(weatherWind);
-  
+        .append(weatherHumidity)
+        .append(weatherWind);
+
     weatherInfo
-      .append(weatherCity)
-      .append(weatherDescription)
-      .append(weatherList);
-  
+        .append(weatherCity)
+        .append(weatherDescription)
+        .append(weatherList);
+
     weatherDiv
-      .append(weatherImage)
-      .append(weatherInfo);
-  
+        .append(weatherImage)
+        .append(weatherInfo);
+
     weatherCard
-      .append(weatherDiv);
-  
+        .append(weatherDiv);
+
     $('#weather-card').append(weatherCard);
-  }
+}
 
 //   displays the forecast
-  var displayForecast = function (data) {
+var displayForecast = function (data) {
     $('#forecast-card').empty();
     var forecastDiv = $('<div>').addClass('row');
     for (let i = 0; i < data.list.length - 34; i++) {
-      console.log(data);
-      var unix = (data.list[i].dt);
-      var icon = (data.list[i].weather[0].icon);
-      var temp = (data.list[i].main.temp)
-      var forecastTime = $('<h5>').addClass('card-title p-2');
-      var forecastCard = $('<div>').addClass('card col-lg-2 col-xs-auto p-2');
-      var forecastImage = $('<img>').addClass('col card-img-top');
-      var forecastDescription = $('<p>').addClass('card-text p-3');
-      var time = new Date((unix) * 1000)
-      forecastTime.text(time.toLocaleString());
-      forecastImage.attr('src', 'http://openweathermap.org/img/wn/' + icon + '@2x.png');
-      forecastDescription.text(temp + '\u00B0');
-  
-      forecastCard
-        .append(forecastTime)
-        .append(forecastImage)
-        .append(forecastDescription);
-  
-      forecastDiv.append(forecastCard);
+        console.log(data);
+        var unix = (data.list[i].dt);
+        var icon = (data.list[i].weather[0].icon);
+        var temp = (data.list[i].main.temp)
+        var forecastTime = $('<h5>').addClass('card-title p-2');
+        var forecastCard = $('<div>').addClass('card col-lg-2 col-xs-auto p-2');
+        var forecastImage = $('<img>').addClass('col card-img-top');
+        var forecastDescription = $('<p>').addClass('card-text p-3');
+        var time = new Date((unix) * 1000)
+        forecastTime.text(time.toLocaleString());
+        forecastImage.attr('src', 'http://openweathermap.org/img/wn/' + icon + '@2x.png');
+        forecastDescription.text(temp + '\u00B0');
+
+        forecastCard
+            .append(forecastTime)
+            .append(forecastImage)
+            .append(forecastDescription);
+
+        forecastDiv.append(forecastCard);
     }
     $('#forecast-card').append(forecastDiv);
-  }
+}
 
 //   gets the current weather
-  var getWeather = function (data) {
+var getWeather = function (data) {
     var lat = (data[0].lat);
     var lon = (data[0].lon);
     var loc = (data[0].state);
     var weather = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
     fetch(weather)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        displayWeather(data, loc);
-        storageAdd(lat, lon);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayWeather(data, loc);
+            storageAdd(lat, lon);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
 
 // this gets the 5day forecast
-  var getForecast = function (data) {
+var getForecast = function (data) {
     var lat = (data[0].lat);
     var lon = (data[0].lon);
     var forecast = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
     fetch(forecast)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        displayForecast(data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            displayForecast(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
 
 //   converts search to coords
-  var getCoordinates = function (q) {
+var getCoordinates = function (q) {
     var locationUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + q + "&appid=" + key;
     fetch(locationUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        getWeather(data);
-        getForecast(data);
-      })
-      .catch(function (err) {
-        console.log(err);
-      })
-  }
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            getWeather(data);
+            getForecast(data);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+
+// displays city with temps
+var displaySearch = function (data) {
+    var city = (data.name);
+    var temp = (data.main.temp);
+    var searchCity = $('<button>').addClass('btn btn-primary border');
+    var searchTemp = $('<span>').addClass('badge text-bg-secondary');
+    searchCity.attr('id', city)
+    searchCity.text(city);
+    searchTemp.text(temp + '\u00B0');
+    searchCity.append(searchTemp);
+    $('#search-list').append(searchCity);
+}
+
 // current weather for city in local
-  var fetchSearch = function () {
+var fetchSearch = function () {
     var getSearch = localStorage.getItem("cities") || '[]';
     var searchItem = JSON.parse(getSearch);
     Object.values(searchItem).forEach(val => {
-      var latilong = val.split(" ");
-      var lat = latilong[0];
-      var lon = latilong[1];
-      var weather = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
-      fetch(weather)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          displaySearch(data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
+        var latilong = val.split(" ");
+        var lat = latilong[0];
+        var lon = latilong[1];
+        var weather = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + key + "&units=imperial";
+        fetch(weather)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                displaySearch(data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     })
-  }
-  
+}
+
 // this is the search button
-  $(document).on('click', '.btn-primary', function (event) {
+$(document).on('click', '.btn-primary', function (event) {
     event.preventDefault();
     if ($(this).attr('id')) {
-      var q = $(this).attr('id');
+        var q = $(this).attr('id');
     } else {
-      var q = $("#searchInput").val();
+        var q = $("#searchInput").val();
     }
     getCoordinates(q);
-  });
-  
-  fetchSearch();
+});
+
+fetchSearch();
