a0bd5a97e8104cb28b3a3d9668da9c108d3457cb
Cory Pendergraft <cory.pendergraft81@gmail.com>
2023-02-10 10:15:19 -0500

    added loacl storage

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,31 @@ var key = "9f0ae0cdf7c18fc91720d7f062d45b91"
 var weatherApi = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + key;
 var geoCoding = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=" + key;
 
+// looks to see if city exists in Local
+var storageCheck = function (searchItems, newCity) {
+    var notMatch = 0
+    Object.values(searchItems).forEach(val => {
+      if (val === newCity) {
+        return;
+      } else {
+        return notMatch++;
+      }
+    })
+    if (notMatch >= searchItems.length) {
+      searchItems.push(newCity);
+      localStorage.setItem("cities", JSON.stringify(searchItems));
+    }
+  };
+
+//   if not, this function adds city to local
+var storageAdd = function (lat, lon) {
+    var getSearches = localStorage.getItem("cities") || '[]';
+    var searchItems = JSON.parse(getSearches);
+    var newCity = (lat + " " + lon);
+    if (searchItems.length > 0) {
+      storageCheck(searchItems, newCity);
+    } else {
+      searchItems.push(newCity);
+      localStorage.setItem("cities", JSON.stringify(searchItems));
+    }
+  }
